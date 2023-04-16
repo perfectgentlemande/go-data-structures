4ecaf950ba516b17fbe2856e4566ad304c865e96
Артем Чаадаев <perfectgentleman.de@ya.ru>
2023-04-16 21:34:52 +0400

    added linked list

--- linkedlist/linkedlist.go
+++ linkedlist/linkedlist.go
@@ -1,1 +1,17 @@
 package linkedlist
+
+import "fmt"
+
+type Node[T int | string] struct {
+	Val  T
+	Next *Node[T]
+}
+
+func (n *Node[T]) PrintList() {
+	cur := n
+
+	for cur != nil {
+		fmt.Println("val: ", cur.Val)
+		cur = cur.Next
+	}
+}
--- main.go
+++ main.go
@@ -1,1 +1,17 @@
 package main
+
+import "github.com/perfectgentlemande/go-data-structures/linkedlist"
+
+func main() {
+	root := &linkedlist.Node[int]{
+		Val: 1,
+		Next: &linkedlist.Node[int]{
+			Val: 2,
+			Next: &linkedlist.Node[int]{
+				Val: 3,
+			},
+		},
+	}
+
+	root.PrintList()
+}
